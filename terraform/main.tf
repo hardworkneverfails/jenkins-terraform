defcc592ec9369d97d9b9f3cd9efcfd700650b6a
hardworkneverfails <93762113+hardworkneverfails@users.noreply.github.com>
2021-11-12 16:49:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "example" {
   ami           = "ami-042e8287309f5df03"
   instance_type = var.instancing
   provisioner "local-exec" { 
-   command = "echo  ${aws_instance_example.private_ip} >> one.txt"
+   command = "echo  ${aws_instance.example.private_ip} >> one.txt"
 }
 }
